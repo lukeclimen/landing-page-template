19483b5ae33e0c575ecb350471019de71e5d6e36
Lucas Climenhage <luke.climenhage@gmail.com>
2024-05-25 11:23:30 -0400

    Add landing page template

---
+++ components/CompanyDescription.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="pt-12 sm:pt-16 lg:pt-20">
+    <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+      On a mission to empower teams
+    </h2>
+    <div class="mt-6 space-y-6 text-gray-500">
+      <p class="text-lg">
+        Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna
+        sed consectetur neque tristique pellentesque. Blandit amet, sed aenean
+        erat arcu morbi. Cursus faucibus nunc nisl netus morbi vel porttitor
+        vitae ut. Amet vitae fames senectus vitae.
+      </p>
+      <p class="text-base leading-7">
+        Sollicitudin tristique eros erat odio sed vitae, consequat turpis
+        elementum. Lorem nibh vel, eget pretium arcu vitae. Eros eu viverra
+        donec ut volutpat donec laoreet quam urna. Sollicitudin tristique eros
+        erat odio sed vitae, consequat turpis elementum. Lorem nibh vel, eget
+        pretium arcu vitae. Eros eu viverra donec ut volutpat donec laoreet quam
+        urna.
+      </p>
+      <p class="text-base leading-7">
+        Rhoncus nisl, libero egestas diam fermentum dui. At quis tincidunt vel
+        ultricies. Vulputate aliquet velit faucibus semper. Pellentesque in
+        venenatis vestibulum consectetur nibh id. In id ut tempus egestas. Enim
+        sit aliquam nec, a. Morbi enim fermentum lacus in. Viverra.
+      </p>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ components/CompanyStats.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="mt-10">
+    <dl class="grid grid-cols-2 gap-x-4 gap-y-8">
+      <div
+        v-for="stat in stats"
+        :key="stat.label"
+        class="border-t-2 border-gray-100 pt-6"
+      >
+        <dt class="text-base font-medium text-gray-500">
+          {{ stat.label }}
+        </dt>
+        <dd class="text-3xl font-bold tracking-tight text-gray-900">
+          {{ stat.value }}
+        </dd>
+      </div>
+    </dl>
+    <div class="mt-10">
+      <a href="#" class="text-base font-medium text-rose-500"
+        >Learn more about how we're changing the world &rarr;</a
+      >
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+const stats = [
+  { label: "Founded", value: "2021" },
+  { label: "Employees", value: "5" },
+  { label: "Beta Users", value: "521" },
+  { label: "Raised", value: "$25M" },
+];
+</script>
+
+<style scoped></style>
