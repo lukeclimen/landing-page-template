d75af0bcd9d470406adada2d74d4d3d9834a947e
Lucas Climenhage <luke.climenhage@gmail.com>
2024-05-25 11:43:05 -0400

    Remove unneeded sections

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,6 @@
   <div class="bg-white py-4">
     <main>
       <PageHero />
-      <!-- Testimonial/stats section -->
       <div class="relative mt-20">
         <div
           class="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-cols-2 lg:items-start lg:gap-24 lg:px-8"
@@ -10,13 +9,9 @@
           <CompanyTestimonial />
           <div class="relative mx-auto max-w-md px-6 sm:max-w-3xl lg:px-0">
             <CompanyDescription />
-            <CompanyStats />
           </div>
         </div>
       </div>
-
-      <InvestorsAdvisors />
-
       <CallToAction />
     </main>
   </div>
