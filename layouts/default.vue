1d892eb52239dd36edd7783cf54833c7c433245f
Lucas Climenhage <luke.climenhage@gmail.com>
2024-06-01 09:21:29 -0400

    Add a layout

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+// const { page } = useContent();
+// useContentHead(page);
+</script>
+
+<template>
+  <main>
+    <div class="bg-white py-4">
+      <slot />
+    </div>
+  </main>
+</template>
