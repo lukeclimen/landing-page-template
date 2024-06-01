382a07985828c5bfd95bc40ff78e375d7203e856
Lucas Climenhage <luke.climenhage@gmail.com>
2024-06-01 09:21:52 -0400

    Create parent component for company details

---
+++ components/content/CompanyDetails.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="relative mt-20">
+    <div
+      class="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-cols-2 lg:items-start lg:gap-24 lg:px-8"
+    >
+      <CompanyTestimonial />
+      <div class="relative mx-auto max-w-md px-6 sm:max-w-3xl lg:px-0">
+        <CompanyDescription />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
